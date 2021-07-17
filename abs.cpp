8c9fdcbf4f89a177ec04f5079bb05541e50ba9e7
Jiaming-tinl <1903009055@qq.com>
2021-07-18 01:31:55 +0800

    主题版

--- abs.cpp
+++ abs.cpp
@@ -64,7 +64,11 @@ void addPerson(struct addressbooks  * abs)
                 abs->personArray[abs->size].sex =  sex;
                 break;
             }
-                cout << "请输入1或2" << endl;
+            else
+            {
+                cout << "参数错误！请输入1或2" << endl;
+            }
+                
 
         }
         
@@ -85,7 +89,7 @@ void addPerson(struct addressbooks  * abs)
 };
 
 // 显示所有联系人
-void displayPerson(addressbooks * abs)
+void displayPerson(struct addressbooks * abs)
 {
     //判断通讯录中人数是否为0
     if (abs->size == 0)
@@ -100,18 +104,128 @@ void displayPerson(addressbooks * abs)
             cout << (abs->personArray[i].sex == 1 ? "男" : "女" ) << "\t";
             cout << abs->personArray[i].age<< "\t";
             cout << abs->personArray[i].phone<< "\t";
-            cout << abs->personArray[i].addr <<endl;
+            cout << abs->personArray[i].addr << endl;
         }
         
     }
     //system("pause");
     //system("clear");
+};
+int isExited(struct addressbooks * abs, string a_name)
+{
+    for (int i=0; i<abs->size; i++)
+    {
+        if (abs->personArray[i].name == a_name)
+        {
+            return i;
+        }
+   
+    }
+    return -1;   //最开始放在循环内部导致错误：IsExited恒返回0
+};
+void deletePerson(struct addressbooks * abs)
+{
+    string d_name;
+    cout << "请输入想删除的联系人："<< endl;
+    cin >> d_name;
+    int ret = isExited(abs, d_name); 
+    if (ret == -1)
+    {
+        cout << "未找到此人" << endl;
+    }
+    else
+    {
+        abs->size --;
+        cout << "正在删除：" << abs->personArray[ret].name << endl;
+        for (int i=ret; i<abs->size; i++)
+        {
+            abs->personArray[i] = abs->personArray[i+1];
+        }
+        cout << "删除完成" << endl;
+
+    }
+};
+void searchPerson(addressbooks * abs)
+{
+    string s_name;
+    cout << "输入查找的联系人" << endl;
+    cin >> s_name;
+    int ret = isExited(abs, s_name);
+    if (ret == -1)
+    {
+        cout <<"未找到此人"<< endl;
+    }
+    else
+    {
+        cout << "查找结果如下："<< endl;
+        cout << abs->personArray[ret].name<< "\t";
+        cout << (abs->personArray[ret].sex == 1 ? "男" : "女" ) << "\t";
+        cout << abs->personArray[ret].age<< "\t";
+        cout << abs->personArray[ret].phone<< "\t";
+        cout << abs->personArray[ret].addr << endl;
+    }
+};
+void modifyPerson(addressbooks * abs)
+{
+     string m_name;
+    cout << "输入要修改的联系人" << endl;
+    cin >> m_name;
+    int ret = isExited(abs, m_name);
+    if (ret == -1)
+    {
+        cout <<"未找到此人"<< endl;
+    }
+    else
+    {
+        cout << "请输入姓名" << endl;
+        cin >> abs->personArray[ret].name;  //"->"用在地址abs，"."用于值
+      
+        cout << "请输入性别" << endl;
+        cout << "1 --- 男" << endl;
+        cout << "2 --- 女" << endl;
+        int sex = 0;
+        while (true)
+        {
+            cin >> sex;     //循环外输入，循环内判断
+            if (sex == 1 || sex == 2)
+            {
+                abs->personArray[ret].sex =  sex;
+                break;
+            }
+            else
+            {
+                cout << "参数错误！请输入1或2" << endl;
+            }
+                
+
+        }
+        
+        cout << "请输入年龄" << endl;
+        cin >> abs->personArray[ret].age;
+
+        cout << "请输入电话" << endl;
+        cin >> abs->personArray[ret].phone;
+
+        cout << "请输入住址" << endl;
+        cin >> abs->personArray[ret].addr;
+
+    }
 }
+void clearPerson(addressbooks * abs)
+{
+    abs->size = 0;
+    cout << "已清空联系人" << endl;
+}
+
+
+
+
+
 
 int main()
 {
   
-
+    string a_name;
     int select = 10;
     addressbooks abs;   //创建通讯录结构体变量
     abs.size = 0;   //初始化通讯录中当前人员个数
@@ -132,13 +246,17 @@ int main()
             case 2:  //显示联系人
                 displayPerson(&abs);
                 break;
-            case 3:
+            case 3:  //删除联系人
+                deletePerson(&abs);
                 break;
-            case 4:
+            case 4:  //查找联系人
+                searchPerson(&abs);
                 break;
-            case 5:
+            case 5:  //修改联系人
+                modifyPerson(&abs);
                 break;
-            case 6:
+            case 6:  //清空联系人
+                clearPerson(&abs);
                 break;
             default:
                 break;
